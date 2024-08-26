a5a4cebf0e7cb60dd92627a259922b6bbe66a046
lilly <lilly.koller@icloud.com>
2024-08-26 14:43:35 +0200

    adding random color api

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
 });
+
+app.get('/color', function(req, res){
+   res.send(randomColor());
+});
+
+function randomColor() {
+   return [Math.floor(Math.random()*(256)), Math.floor(Math.random()*(256)), Math.floor(Math.random()*(256))];
+}
 
 app.listen(3000);
